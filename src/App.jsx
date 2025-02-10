edaa0e98bd1b6a44c69e7bef7ab3b02df772875d
kartiksubaiah <kartiksubaiah@gmail.com>
2025-02-11 02:16:48 +0530

    included import statement for pic

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Bio from "./Bio";
 import Skill from "./skill";
 import { skill } from "./skillobj";
 import "./app.css";
+import ProfPic from "./assets/AlfredPic.png";
 
 const App = () => {
   return (
     <div className="card">
       <h1>Profile Card</h1>
-      <img src="ProfileCardReact/public/Alfred_pic.png" alt="Profile Picture" />
+      <img src={ProfPic} alt="Profile Picture" />
       <div className="container">
         <div className="info">
           <h2>Kartik Subaiah</h2>
